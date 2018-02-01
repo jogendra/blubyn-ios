e9e1689086e034de7a6c062b7ff7658a4cd74b91
imjog <imjog24@gmail.com>
2018-02-01 23:57:42 +0530

    facebook login sdk added

--- Blubyn/ViewController.swift
+++ Blubyn/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 fileprivate enum DefaultValues {
     static let buttonCornerRadiusConstant: CGFloat = 4.0
@@ -16,11 +17,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var logoImageView: UIImageView!
     
-    @IBOutlet weak var facebookLoginButton: UIButton!
+    @IBOutlet weak var facebookLoginButton: FBSDKLoginButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initialUISetups()
+        facebookLoginButton.delegate = self
     }
     
     fileprivate func initialUISetups() {
@@ -28,5 +30,37 @@ class ViewController: UIViewController {
         facebookLoginButton.layer.cornerRadius = DefaultValues.buttonCornerRadiusConstant
     }
 
+    @IBAction func didTapFbLoginButton(_ sender: Any) {
+        let facebookLoginManager = FBSDKLoginManager()
+        facebookLoginManager.logIn(withReadPermissions: ["publicProfile"], from: self, handler: { result, error   in
+            let loginResult: FBSDKLoginManagerLoginResult = result!
+            if error != nil {
+                debugPrint(error!.localizedDescription)
+            } else if loginResult.isCancelled {
+                return
+            } else {
+                
+            }
+        })
+    }
+    
+    // Fetch User's Public Facebook Profile Data
+    fileprivate func fetchUserProfileData() {
+        let params = ["fields": "email, first_name, last_name, picture"]
+        FBSDKGraphRequest(graphPath: "me", parameters: params).start(completionHandler: { connection, result, error in
+            print(result.debugDescription)
+        })
+    }
 }
 
+extension ViewController: FBSDKLoginButtonDelegate {
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    
+}
