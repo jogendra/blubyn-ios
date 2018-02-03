11e0c7e5eff348e5031d7db25f851fc122647668
imjog <imjog24@gmail.com>
2018-02-03 17:53:47 +0530

    initialize profilevc

---
+++ Blubyn/Controllers/ProfileViewController/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Blubyn
+//
+//  Created by JOGENDRA on 03/02/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
