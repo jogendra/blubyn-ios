feec89b3685229d993fe071c0271f06a391a4947
imjog <imjog24@gmail.com>
2018-02-03 22:26:33 +0530

    Profile View implemented

--- Blubyn/Controllers/ProfileViewController/ProfileViewController.swift
+++ Blubyn/Controllers/ProfileViewController/ProfileViewController.swift
@@ -24,27 +24,51 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: JSTextField!
     
+    fileprivate var isEditingMode: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initialUISetups()
         textFieldsEditingMode(isEditable: false)
+        editButtonInitialUISetup()
     }
     
     fileprivate func initialUISetups() {
+        
         editProfileButton.layer.borderWidth = 0.5
         editProfileButton.layer.borderColor = UIColor.black.withAlphaComponent(0.4).cgColor
         editProfileButton.layer.cornerRadius = 0.5 * editProfileButton.frame.width
     }
     
     fileprivate func textFieldsEditingMode(isEditable: Bool) {
+        
         firstNameTextField.isEnabled = isEditable
         lastNameTextField.isEnabled = isEditable
         phoneNumberTextField.isEnabled = isEditable
         emailTextField.isEnabled = isEditable
+        
+    }
+    
+    fileprivate func editButtonInitialUISetup() {
+        
+        let editButtonImage = UIImage(named: "edit-profile")
+        editProfileButton.setImage(editButtonImage, for: .normal)
+        editProfileButton.backgroundColor = UIColor(named: "appThemeColor")
     }
 
     @IBAction func didTapEditProfile(_ sender: Any) {
-        textFieldsEditingMode(isEditable: true)
+        
+        if isEditingMode {
+            editButtonInitialUISetup()
+            textFieldsEditingMode(isEditable: false)
+            isEditingMode = false
+        } else {
+            textFieldsEditingMode(isEditable: true)
+            let saveButtonImage = UIImage(named: "save-profile")
+            editProfileButton.setImage(saveButtonImage, for: .normal)
+            editProfileButton.backgroundColor = UIColor(named: "appGreen")
+            isEditingMode = true
+        }
     }
     
 }
