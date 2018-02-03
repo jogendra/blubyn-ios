09ef4110283821b9981cb39dc8561988e61465ce
imjog <imjog24@gmail.com>
2018-02-03 21:24:21 +0530

    Profile VC ui setups

--- Blubyn/Controllers/ProfileViewController/ProfileViewController.swift
+++ Blubyn/Controllers/ProfileViewController/ProfileViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var editProfileButton: UIButton!
+    
+    @IBOutlet weak var firstNameTextField: JSTextField!
+    
+    @IBOutlet weak var lastNameTextField: JSTextField!
+    
+    @IBOutlet weak var phoneNumberTextField: JSTextField!
+    
+    @IBOutlet weak var emailTextField: JSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        initialUISetups()
+        textFieldsEditingMode(isEditable: false)
+    }
+    
+    fileprivate func initialUISetups() {
+        editProfileButton.layer.borderWidth = 0.5
+        editProfileButton.layer.borderColor = UIColor.black.withAlphaComponent(0.4).cgColor
+        editProfileButton.layer.cornerRadius = 0.5 * editProfileButton.frame.width
+    }
+    
+    fileprivate func textFieldsEditingMode(isEditable: Bool) {
+        firstNameTextField.isEnabled = isEditable
+        lastNameTextField.isEnabled = isEditable
+        phoneNumberTextField.isEnabled = isEditable
+        emailTextField.isEnabled = isEditable
     }
 
+    @IBAction func didTapEditProfile(_ sender: Any) {
+        textFieldsEditingMode(isEditable: true)
+    }
+    
 }
