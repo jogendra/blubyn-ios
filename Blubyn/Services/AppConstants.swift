c5f73278d01379e11305bfcee173bd593f124646
imjog <imjog24@gmail.com>
2018-02-10 20:42:58 +0530

    Added AppConstants

---
+++ Blubyn/Services/AppConstants.swift
@@ -0,0 +1,26 @@
+//
+//  AppConstants.swift
+//  Blubyn
+//
+//  Created by JOGENDRA on 10/02/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import Foundation
+
+struct BlubynImages {
+    
+    static let profileIcon: String = ""
+}
+
+struct BlubynKeys {
+    
+}
+
+struct BlubynEndpoints {
+    
+}
+
+struct BlubynURLs {
+    
+}
