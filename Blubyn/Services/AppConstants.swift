8ba7f3ccbf81ec9a3d2a8fe0eb30b0433fa15eef
imjog <imjog24@gmail.com>
2018-02-10 21:47:58 +0530

    code refactoring

--- Blubyn/Services/AppConstants.swift
+++ Blubyn/Services/AppConstants.swift
@@ -8,19 +8,39 @@
 
 import Foundation
 
+// MARK: - Images names
+
 struct BlubynImages {
     
-    static let profileIcon: String = ""
+    static let profileIcon: String = "profile-icon"
+    static let myTripIcon: String = "mytrip-icon"
+    static let experiencesIcon: String = "eye-icon"
+    static let shareIcon: String = "share-icon"
+    static let helpAndFeedbackIcon: String = "help-and-feedback"
+    static let settingsIcon: String = "settings-icon"
+    static let likeOnFacebookIcon: String = "like-us-on-facebook"
+    static let rateUsIcon: String = "rate-us"
+    static let logoutIcon: String = "logout-icon"
 }
+
+// MARK: - Keys
 
 struct BlubynKeys {
     
 }
 
-struct BlubynEndpoints {
+// MARK: - Endpoints
+
+enum BlubynEndpoints: String {
     
+    case api = "api/"
+    case sendMessage = "sendmessage/"
 }
+
+// MARK: - API URLs
 
 struct BlubynURLs {
     
+    static let testingApiURL: String = "https://test.blubyn.com/"
+    static let productionApiURL: String = "https://app.blubyn.com/"
 }
